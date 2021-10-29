8dc8fe85d79abf755de3586a7a9c825b755a6b9d
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2021-10-29 23:48:19 +0300

    added hbs and fechCountries

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+import '../style.css';
+import CountryCardTpl from '../template/country-card.hbs';
+import API from './fetchCountries';
+import getRefs from './get-refs';
+
+const refs = getRefs();
+
+refs.searchInput.addEventListener("input", onInputChange);
+
+function onInputChange(evt) {
+    evt.preventDefault();
+
+
+const input = evt.currentTarget;
+const searchQuery = input.elements.query.value;
+
+API.fetchCountries(searchQuery)
+    .then(renderCountryCard)
+    .catch(onFetchError)
+    .finally(() => input.reset());
+}
+
+function renderCountryCard(country) {
+    const murkup = CountryCardTpl(country);
+    refs.cardContainer.innerHTML = murkup;
+}
+
+function onFetchError(error) {
+    alert('Такої країни нема!');
+}
