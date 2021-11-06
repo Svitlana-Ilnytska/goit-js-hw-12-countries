9c3666c9b45bae0695bf8653e4a92183a0e33498
Svitlana-Ilnytska <72188793+Svitlana-Ilnytska@users.noreply.github.com>
2021-11-06 12:56:35 +0200

    fixed fetch func

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,13 @@
-const BASE_URL = 'https://restcountries.com'
+const BASE_URL = 'https://restcountries.com';
 
 function fetchCountries(text) {
-    return  fetch(`${BASE_URL}/v2/name/${text}`).then(response =>
-        response.json(),
-        );
+  return fetch(`${BASE_URL}/v2/name/${text}`)
+    .then(response => {
+      if (response.ok) return response.json();
+      throw new Error('Eroor fetching data');
+    })
+    .catch(error => {
+      console.error('Error: ', error);
+    });
 }
 export default { fetchCountries };
